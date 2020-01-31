a46f0b05a1c93b6909f4ec05c56cdebf690ffd09
shraddhaprs <shraddhaprs@gmail.com>
2020-01-30 22:38:29 -0800

    programs

---
+++ 31.01.2020/c# exception/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class DivNums
+    {
+        int result;
+
+        DivNums()
+        {
+            result = 0;
+        }
+        public void division(int num1,int num2)
+        {
+            try
+            {
+                result = num1 / num2;
+            } catch (DivideByZeroException e)
+            {
+                Console.WriteLine("exception caught: {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine("result:{0}", result);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DivNums d = new DivNums();
+            d.division(25, 5);
+            Console.ReadKey();
+        }
+    }
+
+}
